6789665dc44f6d80cc04764c07e8b8eaab06bd50
Patel Harsh <littlemaster455@gmail.com>
2019-11-03 21:12:43 +0530

    FIRST GUI

--- app/src/main/java/com/example/parking/LoginActivity.java
+++ app/src/main/java/com/example/parking/LoginActivity.java
@@ -1,14 +1,25 @@
 package com.example.parking;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     ImageView backButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +27,6 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login2);
 
         backButton=findViewById(R.id.backBtn);
-
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/parking/MainActivity.java
+++ app/src/main/java/com/example/parking/MainActivity.java
@@ -11,9 +11,6 @@ public class MainActivity extends AppCompatActivity {
 
     Button loginButton;
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
